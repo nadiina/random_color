9e264a2d506122e68198cedd3aa5121e50b9ad34
nadiina <nadiia.shcher@gmail.com>
2023-02-08 21:26:16 +0200

    1.0

---
+++ scripts.js
@@ -0,0 +1,22 @@
+const text = document.querySelector('.text');
+const block = document.querySelector('.block');
+const btm = document.querySelector('.btm');
+
+const HEX = '0123456789ABCDEF';
+
+
+const getRandomColor = () => {
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += HEX[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+btm.addEventListener('click', () => {
+    const currentColor = getRandomColor();
+    block.style.backgroundColor = currentColor;
+    block.style.border = 'none';
+
+    text.textContent = currentColor;
+
+})
